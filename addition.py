254e6eb912fd85a3116e1bdce6ebb70c679b48df
sumsh <sumeshmurali46@gmail.com>
2019-09-19 01:08:23 +0530

    Added a new line

---
+++ addition.py
@@ -0,0 +1,3 @@
+first_number = eval(input("Enter first number : "))
+second_number = eval(input("Enter second number : "))
+print(str(first_number) + " + " + str(second_number) + " = " + str(first_number+second_number))
